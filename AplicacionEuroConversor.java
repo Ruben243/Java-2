54449aae4b239e913acbdb94d7faa7c5c2cdae6b
Ruben243 <43281288+Ruben243@users.noreply.github.com>
2019-07-20 15:52:06 +0200

    entorno grafico

---
+++ AplicacionEuroConversor.java
@@ -0,0 +1,11 @@
+package EjerciciosGraficos;
+
+public class AplicacionEuroConversor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EuroConversor v1=new EuroConversor();
+
+	}
+
+}
